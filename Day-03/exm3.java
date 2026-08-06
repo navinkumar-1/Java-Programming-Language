1d316986743303e5b332eb92baf250c166cda88d
navinkumar-1 <gadadenavinkumar@gmail.com>
2026-08-07 00:19:21 +0530

    learned Ternary Operator

---
+++ Day-03/exm3.java
@@ -0,0 +1,17 @@
+public class exm3 {
+    public static void main(String args[]){
+        int a = 1;
+        int b = 3;
+        int c = 6;
+
+        if ((a>b)&&(a>c)){
+            System.out.println("A is Greter ");
+        }
+        else if ((b>a)&&(b>c)){
+            System.out.println("B is Greter");
+        }
+        else{
+            System.out.println("C is Greter");
+        }
+    }
+}
