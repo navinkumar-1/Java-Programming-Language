320780b20968927305c50919f7acc75bb9b76d59
navinkumar-1 <gadadenavinkumar@gmail.com>
2026-08-06 20:52:12 +0530

    Day 2:practice programs

---
+++ Day-02/exm1.java
@@ -0,0 +1,13 @@
+public class exm1 {
+    public static void main(String Args[]){
+        int x=2;
+        int y =5;
+
+        int exp1 = (x*y/x);
+        int exp2 = (x*(y/x));
+
+        System.out.print(exp1 +" , ");;
+        System.out.println(exp2);
+    }
+    
+}
---
+++ Day-02/exm5.java
@@ -0,0 +1,11 @@
+public class exm5 {
+    public static void main(String args[]){
+        int x =10;
+        int y =20;
+
+        int exp1 = (y*(x/y+x/y));
+        int exp2 = (y*x/y+y*x/y);
+        System.out.println(exp1 + "\n" + exp2);
+    }
+    
+}
