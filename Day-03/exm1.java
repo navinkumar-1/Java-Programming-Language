a87d384cfcfa3b06b8a394a0a1a7437db851b4ee
navinkumar-1 <gadadenavinkumar@gmail.com>
2026-08-07 00:04:13 +0530

    Day 3: Started Conditional Statements

---
+++ Day-03/exm1.java
@@ -0,0 +1,14 @@
+public class exm1 {
+    public static void main(String args[]){
+        int a =1;
+        int b = 3;
+
+        if (a>b){
+            System.out.println("Gretest Number is :-"+a);
+        }
+        else{
+            System.out.println("Gretest Number is :-"+b);
+        }
+    }
+    
+}
---
+++ Day-03/first.java
@@ -0,0 +1,12 @@
+public class first {
+    public static void main(String args[]){
+        int a =10;
+        if(a==10){
+            System.out.println("a is 10");
+        }
+        else{
+            System.out.println("a is not 10");
+        }
+    }
+    
+}
